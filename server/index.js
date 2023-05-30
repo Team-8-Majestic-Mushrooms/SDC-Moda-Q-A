73d16c94e233f1c66552102200a10cafd1710ae4
Philip <pwang0407@gmail.com>
2023-05-30 14:21:30 -0700

    comments

--- server/index.js
+++ server/index.js
@@ -13,14 +13,14 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// API Requests
+// API Requests: Questions
 app.get('/qa/questions', controller.getQuestions);
 app.post('/qa/questions', controller.addQuestion);
-app.get('/qa/questions/:question_id/answers', controller.getAnswers);
-app.post('/qa/questions/:question_id/answers', controller.addAnswer);
-
 app.put('/qa/questions/:question_id/helpful', controller.questionHelpful);
 app.put('/qa/questions/:question_id/report', controller.reportQuestion);
+// API Requests: Answers
+app.get('/qa/questions/:question_id/answers', controller.getAnswers);
+app.post('/qa/questions/:question_id/answers', controller.addAnswer);
 app.put('/qa/answers/:answer_id/helpful', controller.answerHelpful);
 app.put('/qa/answers/:answer_id/report', controller.reportAnswer);
 
